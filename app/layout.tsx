acce5959cfb123e9de42e5d6565962ab6e8a1859
Felipe Mullen <felipedmullen@gmail.com>
2024-04-11 09:33:29 -0700

    Project skeleton

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+    title: 'Jamdotdev - Feature Demo',
+    description: 'A hopeful submission to the team at Jam for a developer position.',
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <main className="flex h-screen flex-col items-center justify-between p-24">
+            <div className="max-w-5xl items-center justify-between font-mono text-sm">
+                <p className="border border-gray-300 rounded-xl bg-gray-200 p-4">
+                    home page
+                </p>
+            </div>
+        </main>
+    );
+}
